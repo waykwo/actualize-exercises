2f1d48faa1ff3d0f02a7ee5f4ba5eca2cc723d98
greenman76 <72991933+greenman76@users.noreply.github.com>
2024-07-22 16:43:36 -0400

    Create 113.rb

---
+++ 113.rb
@@ -0,0 +1,20 @@
+# QUESTION 1
+# Write code to store 3 different numbers in three different variables.
+# Then write the code to print the three variables added together.
+first = 23
+second = 345
+third = 2
+puts first + second + third
+
+# QUESTION 2
+# If you run the code below, it will crash due to invalid variable names. 
+# Find the invalid variable names and delete the special characters 
+# (be sure to leave the special characters in the valid variable names alone).
+bob7 = 50
+bob = 50
+bob = 50
+@bob = 50
+bob_ = 50
+_bob = 50
+bob = 50
+$bob = 50
