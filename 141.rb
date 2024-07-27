268c3e59c22f7df6f4d77fdfa9aa5b6d4d052561
greenman76 <72991933+greenman76@users.noreply.github.com>
2024-07-26 21:53:39 -0400

    Create 141.rb

---
+++ 141.rb
@@ -0,0 +1,19 @@
+# QUESTION 1
+# Finish annotating the code below.
+x = 0                                  # 1
+names = ["Saron", "Majora", "Danilo"]  # 2
+names[1] = "Bob"                       # 3
+names[0] = x                           # 4
+pp names                               # 5
+
+# 1: x is 0
+# 2: x is 0, names is ["Saron", "Majora", "Danilo"]
+# 3: x is 0, names is ["Saron", "Bob", "Danilo"]
+# 4: x is 0, names is [0, "Bob", "Danilo"]
+# 5: (prints [0, "Bob", "Danilo"])
+
+
+# QUESTION 2
+# Find and fix the mistakes in the code below.
+items = [3, 2, 3, 2]
+pp items
