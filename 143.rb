59eeaa63bee67d766f9ac5bda26cb2cfc2971e7c
greenman76 <72991933+greenman76@users.noreply.github.com>
2024-07-27 14:03:09 -0400

    Create 143.rb

---
+++ 143.rb
@@ -0,0 +1,17 @@
+# QUESTION 1
+# Start with the hash below. In the following lines, add the populations for 
+# New York City (8.4 million) and San Francisco (800,000).
+city_populations = { "Chicago" => 2700000 }  # (don't change this line)
+# Your code goes here
+
+pp city_populations
+
+
+# QUESTION 2
+# The following code should print "I have 2 dogs!" if the number of dogs
+# in the my_pets hash equals 2. However, the code doesn't work.
+# Explain why the code doesn't work and fix the mistake.
+my_pets = { "dogs" => 2, "cats" => 1, "fish" => 5 }
+if my_pets[2] == "dogs"
+  puts "I have 2 dogs!"
+end
