1d2ad253dfd46ec6d1f3839c16099aa9e87a60b0
greenman76 <72991933+greenman76@users.noreply.github.com>
2024-07-27 13:55:19 -0400

    Update 141.rb

--- 141.rb
+++ 141.rb
@@ -3,14 +3,16 @@
 x = 0                                  # 1
 names = ["Saron", "Majora", "Danilo"]  # 2
 names[1] = "Bob"                       # 3
-names[0] = x                           # 4
-pp names                               # 5
+x = names[2]                           # 4
+names[0] = x                           # 5
+pp names                               # 
 
 # 1: x is 0
 # 2: x is 0, names is ["Saron", "Majora", "Danilo"]
 # 3: x is 0, names is ["Saron", "Bob", "Danilo"]
-# 4: x is 0, names is [0, "Bob", "Danilo"]
-# 5: (prints [0, "Bob", "Danilo"])
+# 4: x is "Danilo", names is ["Saron", "Bob", "Danilo"]
+# 5: x is "Danilo", names is ["Danilo", "Bob", "Danilo"]
+# 6: (prints ["Danilo", "Bob", "Danilo"])
 
 
 # QUESTION 2
