780b30637d43699477b3129271e7969fb28ac337
greenman76 <72991933+greenman76@users.noreply.github.com>
2024-07-27 16:00:10 -0400

    Create 151.rb

---
+++ 151.rb
@@ -0,0 +1,39 @@
+# QUESTION 1
+# Write a method that prints out any message or design that you choose
+# (you can use an ASCII art generator if you want to make it fancy).
+# Then run the method three times to see your message display three times.
+def greeting
+  puts "Hello, how are you today?"
+end
+
+i = 0
+while i < 3
+  greeting
+  i = i + 1
+end
+
+
+# QUESTION 2
+# When you run the code below, it should print lyrics. 
+# However, right now if you run the code, it doesn't print anything. 
+# Add the appropriate line of code to make the code run properly.
+def print_lyrics
+  puts "Now this is a story all about how"
+  puts "My life got flipped turned upside down"
+  puts "And I'd like to take a minute, just sit right there"
+  puts "I'll tell you how I became the prince of a town called Bel-Air"
+end
+
+print_lyrics
+
+
+
+
+
+
+# def print_lyrics
+#   puts "Now this is a story all about how"
+#   puts "My life got flipped turned upside down"
+#   puts "And I'd like to take a minute, just sit right there"
+#   puts "I'll tell you how I became the prince of a town called Bel-Air"
+# end
