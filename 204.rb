a3a8bde48e0abe497b851e72d04fe333be6a9a8d
greenman76 <72991933+waykwo@users.noreply.github.com>
2024-07-28 15:17:01 -0400

    Update 204.rb

---
+++ 204.rb
@@ -0,0 +1,8 @@
+# QUESTION 1
+# Paste this link in your browser to make a copy of the Career Inventory Google document: 
+#     https://docs.google.com/document/d/1XaPaG6QXSQEajkHE1noa5pMfa27qmegSOY_FY6zBoQA/copy  
+# Fill in the answers for each question in the Career Inventory.
+# Change the sharing settings of the document to "Anyone with the link can view" 
+# Copy the link to your Career Inventory Google document and paste it below.
+
+https://docs.google.com/document/d/19VUJuL_zlqhzvP1rHD5G-AFqmCOPg5E2AhB5qMjToQA/edit?pli=1#heading=h.ywv0kijypsb
