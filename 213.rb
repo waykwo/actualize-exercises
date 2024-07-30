47b5663e04eff17dcee6534b9d9b3a15042131c8
waykwo <72991933+waykwo@users.noreply.github.com>
2024-07-29 21:05:46 -0400

    Add exercise solutions

---
+++ 213.rb
@@ -0,0 +1,15 @@
+# QUESTION 1
+# Rewrite the code below to use string interpolation instead of concatenation.
+first_name = "Katherine"
+last_name = "Johnson"
+# puts first_name + " " + last_name + " was a NASA research mathematician."
+puts "#{first_name} #{last_name} was a NASA research mathematician."
+
+
+# QUESTION 2
+# The following code has several mistakes that cause it to crash and not run properly.
+# Fix the mistakes to make sure the code runs properly.
+name = "Salvador"
+age = 37
+# puts name + "is" + age + "years old and goes by "Chava" with his friends".
+puts "#{name} is #{age} years old and goes by \"Chava\" with his friends."
