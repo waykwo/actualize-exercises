4453f4217b2f29d67681e4d52a81268485b0859b
waykwo <72991933+waykwo@users.noreply.github.com>
2024-07-29 20:09:29 -0400

    Add exercise solutions

---
+++ 211.rb
@@ -0,0 +1,15 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# 8. Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+number = 3.14159265
+puts number.to_i
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I was able to solve the problem from memory. I plan to spend some time on 
+# Chapter 7 of Learn to Program (https://pine.fm/LearnToProgram/chap_07.html), 
+# especially the each iterator.
