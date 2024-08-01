faf4e9eb544c678de9e9b17411144c050fba9574
waykwo <72991933+waykwo@users.noreply.github.com>
2024-07-31 21:33:53 -0400

    add exercise solutions

---
+++ 231.rb
@@ -0,0 +1,19 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# 2. Start with an array of strings and create a new array with each string upcased.
+# For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+strings_1 = ["hello", "goodbye"]
+strings_2 = []
+strings_1.each do |string|
+    strings_2.append(string.upcase)
+end
+pp strings_2
+
+# Were you able to easily solve the problem from memory?
+#   Yes
+# If so, describe the next skill you will start to practice tomorrow.
+#   I'm going to go back to doing the exercises in order. I only skipped ahead 
+#   a bit because I was reading about each loops and wanted some practice.
+# If not, describe the part you got stuck on that requires more practice.
