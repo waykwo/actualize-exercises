79ab5ad25b9a1b9cac1607165d778dfc02cbd79e
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-01 17:53:38 -0400

    Add exercise solutions

---
+++ 242.rb
@@ -0,0 +1,17 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+words = ["first", "second", "third"]
+words.push("fourth")
+pp words
+
+
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+dictionary = {
+    "zoon" => "An individual animal produced from an egg.",
+    "cobber" => "A pal; a buddy"
+}
+dictionary["diamante"] = "decorated with rhinestones or with other brightly glittering bits of material"
+puts dictionary
