4a6d3c859e192da2a632a5b5deb14dbbcea35c7d
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-01 17:34:48 -0400

    Add exercise solutions

---
+++ 241.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+### 01-variables2.md
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+first = "Sherlock"
+last = "Holmes"
+puts "#{first} #{last}"
+
+# Were you able to easily solve the problem from memory?
+#   Yes
+# If so, describe the next skill you will start to practice tomorrow.
+#   Conditionals
+# If not, describe the part you got stuck on that requires more practice.
+
+
