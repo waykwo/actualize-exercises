56220b30c20959982d3c0eadd3af9a5617220474
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-06 14:09:32 -0400

    Update 304.rb

---
+++ 304.rb
@@ -0,0 +1,8 @@
+# QUESTION 1
+# Log into your LinkedIn account. (If you don't have one, signup for an account).
+#     https://www.linkedin.com/ 
+# Follow this document to improve your LinkedIn profile strength to at least Intermediate:
+#     https://docs.google.com/document/d/1df31q78ZlLuv-1sCQw-p61p-6Tfewf8seuQ5XhE99pM 
+# Paste the link to your LinkedIn profile below.
+https://www.linkedin.com/in/waynekwok/
+
