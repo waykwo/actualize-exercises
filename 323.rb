09846bfbb55d0d7d48226e8286cd6cad841c17c4
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-06 20:14:14 -0400

    Add exercise solutions

---
+++ 323.rb
@@ -0,0 +1,11 @@
+# QUESTION 1
+# Create a program that multiplies by 2 until 100,000. 
+# That is, the program should print out: 1, 2, 4, 8, 16, 32, etc. until you get above 100,000.
+multiplicand = 1
+p multiplicand
+while multiplicand <= 100000
+  multiplicand = multiplicand * 2
+  p multiplicand
+end
+
+
