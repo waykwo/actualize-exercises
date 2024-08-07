0ace4f779bb4c3ea76ac23dbae0660b953f2aa42
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-06 20:02:08 -0400

    Add exercise solutions

---
+++ 322.rb
@@ -0,0 +1,12 @@
+# QUESTION 1
+# Use a loop to create a new array of strings from each hash's :name key.
+dogs = [
+  { name: "Toto", breed: "Cairn Terrier" },
+  { name: "Snoopy", breed: "Beagle" },
+  { name: "Beethoven", breed: "Saint Bernard" }
+]
+names = ""
+dogs.each do |dog|
+  names << dog[:name]
+end
+pp names
