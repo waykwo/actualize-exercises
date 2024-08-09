1885ce0b98a00c3ae0919c49fce5371b957894c5
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-09 18:53:39 -0400

    Add exercise solutions

---
+++ 343.rb
@@ -0,0 +1,17 @@
+# QUESTION 1
+# Create a program that asks the user to enter any word.
+# Then print out the amount of points the word is worth in the game of Scrabble.
+# You can start with the hash below to determine how many points each letter is worth.
+# (As an example, the word "bad" is worth 3 + 1 + 2 = 6 points.)
+letter_points = {'a'=> 1, 'b'=> 3, 'c'=> 3, 'd'=> 2, 'e'=> 1, 'f'=> 4, 'g'=> 2, 'h'=> 4, 'i'=> 1, 'j'=> 8, 'k'=> 5, 'l'=> 1, 'm'=> 3, 'n'=> 1, 'o'=> 1, 'p'=> 3, 'q'=> 10, 'r'=> 1, 's'=> 1, 't'=> 1, 'u'=> 1, 'v'=> 4, 'w'=> 4, 'x'=> 8, 'y'=> 4, 'z'=> 10}
+letters_scores = []
+
+puts "Enter a word here and I will tell you the Scrable word score for that word."
+word = gets.chomp
+# word = "eagle"
+
+letters_array = word.split(//)
+letters_array.each do |letter|
+  letters_scores << letter_points[letter]
+end
+puts "The word is worth #{letters_scores.sum} points in Scrabble!"
