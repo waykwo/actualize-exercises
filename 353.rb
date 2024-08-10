193847f3438f2c314b5d433ab897f02cfc8b6df7
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-10 14:23:08 -0400

    Add exercise solutions

---
+++ 353.rb
@@ -0,0 +1,8 @@
+# QUESTION 1
+# Start your program with the line of code below.
+# The rest of your program should print out the second letter from each inner array. 
+# (The result in your terminal should be the letters b, d, f, h, j on separate lines)
+letters = [["a", "b"], ["c", "d"], ["e", "f"], ["g", "h"], ["i", "j"]]
+letters.each do |letter_group|
+  puts letter_group[1]
+end
