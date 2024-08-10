812baeb6007c8ed341ad251b3b8bdbdad39284ae
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-10 13:54:14 -0400

    Add exercise solutions

---
+++ 351.rb
@@ -0,0 +1,58 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# I wanted to improve on my answer in 334:
+# QUESTION 1
+# Have the user enter 10 words, and allow for duplicate words. 
+# After the user is done, the program will tell the user 
+# which word was entered the most frequently.
+# For example, if the user enters: 
+#   apple, banana, orange, pear, apple, pear, apple, squash, apple, pear
+# The program will say: "apple was your most common word"
+# (That's because apple appeared in the user's list 4 times, more than any other word.)
+
+# Ask user for 10 words and store the words in an array
+words = []
+puts "You will be asked for ten words. Enter some duplicates and I'll tell you which words were entered most frequently."
+10.times do
+  puts "Please enter a word."
+  words << gets.chomp
+end
+
+# Create a hash of the word and the count
+word_count = {}
+words.each do |word|
+  if word_count.include?(word)
+    word_count[word] += 1
+  else
+    word_count[word] = 1
+  end
+end
+# pp word_count
+
+# Get the highest count
+highest_count = word_count.values.max
+
+# Lookup word by highest count
+top_words = word_count.select {|word, count| count == highest_count}
+# pp top_words.keys.join(", ")
+
+# Print highest word
+if highest_count == 1
+  puts "There were no duplicates. All words were unique."
+elsif top_words.length > 1
+  puts "Theses were tied for you most common words: #{top_words.keys.join(", ")}"
+else
+  puts "Your most common word was #{top_words.keys.join("")}."
+end
+
+
+# Were you able to easily solve the problem from memory?
+#   No, I referenced the lecture where Kevin counted the bad apples and felt 
+#   that was a better way to solve this problem.
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+#   It would be good to have more practice counting the values of hashes.
+
+
