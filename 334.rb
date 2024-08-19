ad749a8b435cf5d56375fae083e032f07037ba23
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-18 23:08:56 -0400

    Add exercise solutions

--- 334.rb
+++ 334.rb
@@ -145,3 +145,41 @@ if highest.count > 1
 else
   puts "#{highest[0][:word]} was your most common word"
 end
+
+
+
+### Another solution
+# Ask user for 10 words and store the words in an array
+words = []
+puts "You will be asked for ten words. Enter some duplicates and I'll tell you which words were entered most frequently."
+10.times do
+  puts "Please enter a word."
+  words << gets.chomp
+end
+
+# Create a hash of the word and the count
+word_count = {}
+words.each do |word|
+  if word_count.include?(word)
+    word_count[word] += 1
+  else
+    word_count[word] = 1
+  end
+end
+# pp word_count
+
+# Get the highest count
+highest_count = word_count.values.max
+
+# Lookup word by highest count
+top_words = word_count.select {|word, count| count == highest_count}
+# pp top_words.keys.join(", ")
+
+# Print highest word
+if highest_count == 1
+  puts "There were no duplicates. All words were unique."
+elsif top_words.length > 1
+  puts "Theses were tied for you most common words: #{top_words.keys.join(", ")}"
+else
+  puts "Your most common word was #{top_words.keys.join("")}."
+end
--- 344.rb
+++ 344.rb
@@ -364,3 +364,36 @@ pp $roman_symbols
 $integer = $remainder
 
 puts $roman_symbols.join()
+
+
+
+### Final solution
+values_and_symbols = {
+  1000 => "M",
+  900 => "CM",
+  500 => "D",
+  400 => "CD",
+  100 => "C",
+  90 => "XC",
+  50 => "L",
+  40 => "XL",
+  10 => "X",
+  9 => "IX",
+  5 => "V",
+  4 => "IV",
+  1 => "I"
+}
+
+puts "Please enter an integer:" 
+integer = gets.chomp.to_i
+roman_symbol = ""
+remainder = 0
+values_and_symbols.each do |key, value|
+  if integer >= key
+    multiple = (integer - integer.remainder(key)) / key
+    roman_symbol << value * multiple
+    remainder = integer.remainder(key)
+    integer = remainder
+  end
+end
+pp roman_symbol, remainder
