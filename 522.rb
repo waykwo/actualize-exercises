422b02d5a0e040a3d315413890c167e4504cf4ee
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-20 10:16:18 -0400

    Add exercise solutions

---
+++ 522.rb
@@ -0,0 +1,20 @@
+# QUESTION 1
+# Write a method that takes in the array of hashes below 
+# and returns a new array of strings from the model of each item. 
+# Then run the method and print the result.
+cars = [
+  { "make" => "Toyota", "model" => "Corolla", "year" => 2018 },
+  { "make" => "Honda", "model" => "Civic", "year" => 2020 },
+  { "make" => "Ford", "model" => "Mustang", "year" => 2019 }
+]
+
+def get_model(array)
+  models = []
+  array.each do |item|
+    models.push(item["model"])
+  end
+  return models
+end
+
+models = get_model(cars)
+pp models
