132c3f4684003930b1321c84ab19ad01f5938608
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-23 09:10:18 -0400

    Add exercise solutions

---
+++ 543.js
@@ -0,0 +1,20 @@
+// QUESTION 1
+// Write code to store an array of numbers.
+// The print out the last item in that array.
+const array = [15, 33, 24, 82, 5];
+console.log(array);
+
+// QUESTION 2
+// Write code to store a number in a variable.
+// Then write a condition that 
+// prints -1 if the number is less than 10, 
+// prints 1 if the number is greater than 10, 
+// and prints 0 if the number is equal to 10.
+const number = 15;
+if (number < 10) {
+    console.log(-1)
+} else if (number > 10) {
+    console.log(1)
+} else {
+    console.log(0)
+};
